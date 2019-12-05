3bd03aae2c3f1f526f68a9b842526bc7a3339d9d
webberc <webberc@augsburg.edu>
2019-12-04 22:32:10 -0600

    having trouble putting data from MongoDB into table

--- apps/Tables/routes/InventoryTables.js
+++ apps/Tables/routes/InventoryTables.js
@@ -25,4 +25,52 @@ getInventoryTables = async (req, res) => {
   );
 };
 
+// Routes for manipulating data in Mongo DB
+const url = process.env.MONGO_DB_1_URL;
+
+router.get('/get-data', function(req, res, next){
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('inventory').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('InventoryTable', {items: resultArray});
+    });
+  });
+});
+router.post('/insert', function(req, res, next){
+    var item = {
+        id: req.body.id,
+        type: req.body.type,
+        description: req.body.description,
+        length: req.body.length,
+        width: req.body.width,
+        scrap: req.body.scrap,
+        lastUpdate: req.body.lastUpdate,
+        employee: req.body.employee
+    };
+
+    mongo.connect(url, function(err, db){
+      assert.equal(null, err);
+      db.collection('inventory').insertOne(item, function(err, result) {
+        assert.equal(null, err);
+        console.log('Item inserted');
+        db.close();
+      });
+    });
+
+    res.redirect('/InventoryTable');
+});
+//router.post('/update', function(req, res, next){
+
+//});
+//router.post('/delete', function(req, res, next){
+
+//});
+
+
 module.exports.routes = router;
